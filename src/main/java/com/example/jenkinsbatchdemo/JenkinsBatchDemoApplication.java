e029acbaabf89627f52e01a1010476ec12737570
doyupk <dy.kim@elwide.com>
2023-02-02 15:34:32 +0900

    init

---
+++ src/main/java/com/example/jenkinsbatchdemo/JenkinsBatchDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.jenkinsbatchdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsBatchDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JenkinsBatchDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/jenkinsbatchdemo/JenkinsBatchDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jenkinsbatchdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsBatchDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
